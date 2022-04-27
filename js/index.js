fb468a7234aa3bb3f3963b04cc621b616e5ae90c
GASOMINALI Olivier <ogasominali@gmail.com>
2022-04-27 15:01:56 +0200

    Refactor to use JavaScript classes

---
+++ js/index.js
@@ -0,0 +1,95 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class Display {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => Display.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const bookDisplay = document.createElement('div');
+    bookDisplay.className = 'bookList1';
+    bookDisplay.innerHTML = `
+        <p class="bookTitle">${book.title}</p>
+        <p>by<span></span>${book.author}</p>
+        <button class="delete">Remove</button>
+        `;
+
+    list.appendChild(bookDisplay);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+  }
+}
+
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(title) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Display.displayBooks);
+
+document.querySelector('#book-form').addEventListener('submit', (x) => {
+ 
+  x.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  const book = new Book(title, author);
+
+  Display.addBookToList(book);
+
+  Store.addBook(book);
+
+  Display.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (x) => {
+ 
+  Display.deleteBook(x.target);
+
+  Store.removeBook(x.target.previousElementSibling.previousElementSibling.textContent);
+});
