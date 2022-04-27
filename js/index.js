de3bdeefad7fce7d66a44ab2195730e287e52a63
GASOMINALI Olivier <ogasominali@gmail.com>
2022-04-27 23:30:36 +0200

    Fix Eslint errors

--- js/index.js
+++ js/index.js
@@ -74,18 +74,12 @@ class Store {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
- 
   e.preventDefault();
-
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
-
   const book = new Book(title, author);
-
   UI.addBookToList(book);
-
   Store.addBook(book);
-
   UI.clearFields();
 });
 
